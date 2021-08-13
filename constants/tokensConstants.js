91abe79eac13f008e7d151ea62f7b7f6e5f35753
CentralMatthew <matvij379@gmail.com>
2021-08-13 18:02:15 +0300

    remove constants

--- constants/tokensConstants.js
+++ constants/tokensConstants.js
@@ -3,7 +3,6 @@ module.exports = {
     ACCESS_TOKEN: 'access_token',
     EXPIRES_FOR_ACCESS: '30m',
     EXPIRES_FOR_REFRESH: '30d',
-    EXPIRES_FOR_SIGN: '2h',
     REFRESH: 'refresh',
     REFRESH_TOKEN: 'refresh_token',
 };
